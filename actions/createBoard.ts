ff751bb08b98933a7cb414afeb5a345e158823c5
Appz-dk <patrick.fyhn@gmail.com>
2023-11-17 23:04:39 +0100

    UI updates on board creation

--- actions/createBoard.ts
+++ actions/createBoard.ts
@@ -1,6 +1,7 @@
 "use server"
 
 import { db } from "@/lib/db"
+import { revalidatePath } from "next/cache"
 import { z } from "zod"
 
 const boardSchema = z.object({
@@ -19,4 +20,6 @@ export const createBoard = async (formData: FormData) => {
       title
     }
   })
+
+  revalidatePath("/organization/org_2YDIZMLl9TpsRQAvjW0xFH4udSU")
 }
