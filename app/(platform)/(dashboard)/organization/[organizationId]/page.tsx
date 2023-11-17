993f307bd2d5c80a5a61fabc76f6d943c62a2ff8
Appz-dk <patrick.fyhn@gmail.com>
2023-11-17 20:02:38 +0100

    Display board titles from db

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,14 +1,21 @@
 import { createBoard } from "@/actions/createBoard"
 import { Button } from "@/components/ui/button"
+import { db } from "@/lib/db"
 
-const OrganizationIdPage = () => {
+const OrganizationIdPage = async () => {
+  const boards = await db.board.findMany()
 
   return (
-    <div>
+    <div className="space-y-4">
       <form action={createBoard}>
         <input id="title" name="title" placeholder="Enter title" className="border border-input p-1"/>
         <Button type="submit" className="ml-2">Submit</Button>
       </form>
+      <div className="space-y-1">
+        {boards.map(board => (
+          <div key={board.id}>Board title: {board.title}</div>
+        ))}
+      </div>
     </div>
   )
 }
