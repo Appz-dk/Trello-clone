fb0015b40198b06836658091408741621116240e
Appz-dk <patrick.fyhn@gmail.com>
2023-11-19 21:38:09 +0100

    feat: added suspense with fallback to BoardList

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react"
 import { BoardList } from "./_components/board-list"
 import { Info } from "./_components/info"
 import { Separator } from "@/components/ui/separator"
@@ -10,7 +11,9 @@ const OrganizationIdPage = async () => {
       <Info />
       <Separator className="my-4" />
       <div className="px-2 md:px-4">
-        <BoardList />
+        <Suspense fallback={<BoardList.Skeleton />}>
+          <BoardList />
+        </Suspense>
       </div>
     </div>
   )
