c5b3f9995a7b8c7337a7e625eede6d8e06ffd465
Appz-dk <patrick.fyhn@gmail.com>
2023-11-19 20:54:45 +0100

    feat: Added FormPopover to navbar

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -5,18 +5,22 @@ import { Button } from "@/components/ui/button"
 import { Plus } from "lucide-react"
 
 import { MobileSidebar } from "./mobile-sidebar"
+import { FormPopover } from "@/components/form/form-popover"
 
 export const Navbar = () => {
   return (
     <nav className="fixed z-50 top-0 h-14 w-full px-4 bg-white flex items-center justify-between border-b shadow-sm">
-      {/* TODO: Create mobile sidebar */}
       <div className="flex gap-4 items-center">
         <MobileSidebar />
         <Logo />
-        <Button variant="primary" size="sm" className="hidden md:block rounded-sm">Create</Button>
-        <Button variant="primary" size="sm" className="md:hidden rounded-sm">
-          <Plus className="h-4 w-4" />
-        </Button>
+        <FormPopover align="start" sideOffset={5} side="bottom">
+            <Button variant="primary" size="sm" className="hidden md:block rounded-sm">Create</Button>
+        </FormPopover>
+        <FormPopover sideOffset={5}>
+          <Button variant="primary" size="sm" className="md:hidden rounded-sm">
+            <Plus className="h-4 w-4" />
+          </Button>
+        </FormPopover>
       </div>
       <div className="flex gap-4 items-center">
         <OrganizationSwitcher 
