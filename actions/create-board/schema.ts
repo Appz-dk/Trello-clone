56268f597249015c71acb19e5f287ba60f657fb8
Appz-dk <patrick.fyhn@gmail.com>
2023-11-19 20:17:21 +0100

    feat: Update schema & types for createBoard action

--- actions/create-board/schema.ts
+++ actions/create-board/schema.ts
@@ -6,5 +6,34 @@ export const boardSchema = z.object({
     invalid_type_error: "Title is required"
   }).min(3, {
     message: "Title should be min 3 chars"
+  }),
+  image: z.string({
+    required_error: "Image is required",
+    invalid_type_error: "Image is required"
   })
-}) 
+})
+
+
+
+export const imageSchema = z.object({
+  imageId: z.string({
+    required_error: "Image id is required, but is missing",
+    invalid_type_error: "Image id is required, but is missing"
+  }),
+  imageThumbUrl: z.string({
+    required_error: "Image thumb url is required, but is missing",
+    invalid_type_error: "Image thumb url is required, but is missing"
+  }),
+  imageFullUrl: z.string({
+    required_error: "Image full url is required, but is missing",
+    invalid_type_error: "Image full url is required, but is missing"
+  }),
+  imageLinkHTML: z.string({
+    required_error: "Image HTML link is required, but is missing",
+    invalid_type_error: "Image HTML link is required, but is missing"
+  }),
+  imageUserName: z.string({
+    required_error: "Image username is required, but is missing",
+    invalid_type_error: "Image username is required, but is missing"
+  }),
+})
--- actions/create-board/types.ts
+++ actions/create-board/types.ts
@@ -2,7 +2,9 @@ import { z } from "zod"
 import { Board } from "@prisma/client"
 
 import { ActionState } from "@/lib/create-safe-action"
-import { boardSchema } from "./schema"
+import { boardSchema, imageSchema } from "./schema"
 
 export type InputType = z.infer<typeof boardSchema>
 export type ReturnType = ActionState<InputType, Board>
+
+export type ImageData = z.infer<typeof imageSchema>
