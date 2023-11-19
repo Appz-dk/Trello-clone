2117f40ced0c16612071c21a260410a13eaeb532
Appz-dk <patrick.fyhn@gmail.com>
2023-11-19 22:43:52 +0100

    feat: change page title to include org name

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,8 +1,18 @@
+import { startCase } from "lodash"
+
 import { OrgControl } from "./_components/org-control"
+import { auth } from "@clerk/nextjs"
 
 type Props = {
   children: React.ReactNode
 }
+
+export async function generateMetadata() {
+  const { orgSlug } = auth()
+  return {
+    title: startCase(orgSlug || "organization")
+  };
+};
 
 const OrganizationIdLayout: React.FC<Props> = ({children}) => {
   return (
