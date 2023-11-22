9f87ac76284ac2bc7c8698962c8e00aed25a2957
Appz-dk <patrick.fyhn@gmail.com>
2023-11-23 00:04:03 +0100

    feat: Added list container component

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -2,6 +2,8 @@
 
 import { ListWithCards } from "@/types/types";
 import { ListForm } from "./list-form";
+import { useEffect, useState } from "react";
+import { ListItem } from "./list-item";
 
 type Props = {
   boardId: string;
@@ -9,9 +11,21 @@ type Props = {
 }
 
 export const ListContainer = ({ boardId, lists }: Props) => {
+  // Local source of truth (for better drag & drop experience)
+  const [orderedListData, setOrderedListData] = useState(lists)
+
+  // Keep listData and lists in sync
+  useEffect(() => {
+    setOrderedListData(lists)
+  }, [lists])
+
   return (
-    <ol className="">
+    <ol className="flex h-full gap-3">
+      {orderedListData.map((list, idx) => (
+        <ListItem key={list.id} index={idx} data={list} />
+      ))}
       <ListForm />
+      {/* Spacer div */}
       <div className="w-1 flex-shrink-0" />
     </ol>
   )
