b2c88c82c4d4fb0149991fb4cebcc5a941634b2f
Appz-dk <patrick.fyhn@gmail.com>
2023-11-25 00:52:05 +0100

    feat: added Button variant

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -19,13 +19,15 @@ const buttonVariants = cva(
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
         primary: "text-primary-foreground bg-sky-700 hover:bg-sky-700/80",
-        transparent: "bg-transparent text-white hover:bg-white/20 border"
+        transparent: "bg-transparent text-white hover:bg-white/20 border",
+        gray: "text-secondary-foreground bg-neutral-200 hover:bg-neutral-300"
       },
       size: {
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
+        inline: "h-auto text-sm px-2 py-1.5"
       },
     },
     defaultVariants: {
