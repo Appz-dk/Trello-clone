1395fe665527c3de5ef98cd8c85d5ccb6d86d3ff
Appz-dk <patrick.fyhn@gmail.com>
2023-11-24 18:14:38 +0100

    feat: Added modal provider component

---
+++ components/providers/modal-provider.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import { useEffect, useState } from "react"
+
+import { CardModal } from "@/components/modals/card-modal/card-modal"
+
+
+export const ModalProvider = () => {
+  // Protection from hydration errors
+  const [isMounted, setIsMounted] = useState(false)
+
+  useEffect(() => {
+    setIsMounted(true)
+  })
+
+  // Only render this component on the client (useEffect only runs on the client)
+  if (!isMounted) return null
+
+  return (
+    <>
+    <CardModal />
+    </>
+  )
+}
