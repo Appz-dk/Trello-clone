b6c5cc1666148a30f9c1f8bec1415173181b3a91
Appz-dk <patrick.fyhn@gmail.com>
2023-11-25 01:23:40 +0100

    feat: added card modal content

--- components/modals/card-modal/card-modal.tsx
+++ components/modals/card-modal/card-modal.tsx
@@ -8,6 +8,8 @@ import { useCardModal } from "@/hooks/use-card-modal"
 
 import { Dialog, DialogContent } from "@/components/ui/dialog"
 import { CardModalHeader } from "./card-modal-header"
+import { CardModalDescription } from "./card-modal-description"
+import { CardModalActions } from "./card-modal-actions"
 
 export const CardModal = () => {
   const { isOpen, onClose, id } = useCardModal(state => state)
@@ -19,11 +21,21 @@ export const CardModal = () => {
   if (!cardData) return null
 
   const modalHeaderContent = cardData ? <CardModalHeader data={cardData} /> : <CardModalHeader.Skeleton />
+  const modalDescriptionContent = cardData ? <CardModalDescription data={cardData} /> : <CardModalDescription.Skeleton />
+  const modalActionContent = cardData ? <CardModalActions data={cardData} /> : <CardModalActions.Skeleton />
 
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogContent >
-        {modalHeaderContent}
+      <DialogContent>
+        <div className="max-w-[440px]">
+          {modalHeaderContent}
+          <div className="grid grid-cols-1 md:grid-cols-4 md:gap-4">
+            <div className="col-span-3">
+              {modalDescriptionContent}
+            </div>
+            {modalActionContent}
+          </div>
+        </div>
       </DialogContent>
     </Dialog>
   )
